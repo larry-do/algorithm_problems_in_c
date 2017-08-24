cb1241d1d5cb2ed36af95017c4bfb9414ad16e68
Dou_Ding_Nyat <nhatkap2@gmail.com>
2017-08-23 22:57:00 -0700

    a

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#Bài tập luyện tập C
+﻿#Bài tập luyện tập C
+
+Các bài còn nhiều lỗi hay cách giải ngu ngu do đây là bài lúc mình nhập môn
--- spiralFibonacyMatrix.c
+++ spiralFibonacyMatrix.c
@@ -1,86 +1,31 @@
 //matrix fibonacy xoan oc
 #include<stdio.h>
 #define MAX 100
-int main()
+void main()
 {
-    int a[MAX][MAX],n,fibo[1000];
+    int a[MAX][MAX];
+    int n=3;
     int i,j;
-    int hang_tang,hang_giam,cot_tang,cot_giam;
-    int row,col;
-    int chi_so;
-    scanf("%d",&n);
-    hang_tang=cot_tang=n-1;
-    hang_giam=cot_giam=0;
-    row=col=0;
-    chi_so=1;
     //tao ra mot mang chua cac so fibo truoc
+    int fibo[MAX];
     fibo[0]=1;
     fibo[1]=1;
-    for(i=2;i<n*n;i++)
-    {
+    for(i=2;i<n*n;i++){
         fibo[i]=fibo[i-1]+fibo[i-2];
     }
     //bat dau
-    for(i=0;i<n*n;i++)
-    {
-        a[row][col]=fibo[i];
-        switch(chi_so)
-        {
-        case 1:
-            {
-                if(col<cot_tang)
-                    col++;
-                else
-                {
-                    chi_so++;
-                    hang_giam++;
-                    row++;
-                }
-                break;
-            }
-        case 2:
-            {
-                if(row<hang_tang)
-                    row++;
-                else
-                {
-                    chi_so++;
-                    cot_tang--;
-                    col--;
-                }
-                break;
-            }
-        case 3:
-            {
-                if(col>cot_giam)
-                    col--;
-                else
-                {
-                    chi_so++;
-                    hang_tang--;
-                    row--;
-                }
-                break;
-            }
-        case 4:
-            {
-                if(row>hang_giam)
-                    row--;
-                else
-                {
-                    chi_so=1;
-                    cot_giam++;
-                    col++;
-                }
-                break;
-            }
+    int cur=0;
+    for(i=0;i<(n+1)/2;++i){
+        for(j=i;j<n-i-1;++j) a[i][j]=fibo[cur++];
+        for(j=i;j<n-i-1;++j) a[j][n-i-1]=fibo[cur++];
+        for(j=n-i-1;j>i;--j) a[n-i-1][j]=fibo[cur++];
+        for(j=n-i-1;j>i;--j) a[j][i]=fibo[cur++];
+    }
+    if(n&1) a[n/2][n/2]=fibo[n*n-1];
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%4d",a[i][j]);
         }
+        printf("%c",'\n');
     }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-            printf("%d ",a[i][j]);
-        printf("\n");
-    }
-    return 0;
 }
